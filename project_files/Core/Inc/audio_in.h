d9328a1f015127e1ce1ad8bcb4e76a330480b8e5
Alex Linux LENY6M22 <opportunity-alex@mail.de>
2026-05-07 14:56:26 +0200

    audio_in Code jetzt wirklich hinzugefügt

--- project_files/Core/Inc/audio_in.h
+++ project_files/Core/Inc/audio_in.h
@@ -1,1 +1,13 @@
+#ifndef AUDIO_IN_H
+#define AUDIO_IN_H
 
+#include "main.h"
+
+#define AUDIO_BUFFER_SIZE 4096
+
+extern int16_t audioBuffer[AUDIO_BUFFER_SIZE];
+
+void AUDIO_IN_Init(void);
+void AUDIO_IN_Start(void);
+
+#endif
--- project_files/Core/Src/audio_in.c
+++ project_files/Core/Src/audio_in.c
@@ -1,1 +1,35 @@
 
+#include "audio_in.h"
+#include "stm32746g_discovery_audio.h"
+
+int16_t audioBuffer[AUDIO_BUFFER_SIZE];
+
+static uint32_t AudioFreq = AUDIO_FREQUENCY_44K;
+
+void AUDIO_IN_Init(void)
+{
+    BSP_AUDIO_IN_Init(
+        INPUT_DEVICE_INPUT_LINE_1,
+        AudioFreq,
+        DEFAULT_AUDIO_IN_BIT_RESOLUTION,
+        DEFAULT_AUDIO_IN_CHANNEL_NBR
+    );
+
+    BSP_AUDIO_IN_SetVolume(80);
+}
+
+void AUDIO_IN_Start(void)
+{
+    BSP_AUDIO_IN_Record((uint16_t*)audioBuffer, AUDIO_BUFFER_SIZE);
+}
+
+// DMA callbacks
+void BSP_AUDIO_IN_HalfTransfer_CallBack(void)
+{
+    // Erste Hälfte auswerten
+}
+
+void BSP_AUDIO_IN_TransferComplete_CallBack(void)
+{
+    // Zweite Hälfte auswerten
+}
